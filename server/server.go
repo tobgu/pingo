10ed84f6dc2ee3f044543cc8b7de2b437b2595ec
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2017-03-12 12:17:58 +0100

    UDP echo server

--- server/server.go
+++ server/server.go
@@ -1,6 +1,42 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"github.com/pkg/errors"
+	"log"
+)
+
+
+func echoUdp(conn *net.UDPConn) {
+	defer conn.Close()
+	buf := make([]byte, 2048)
+	for {
+		n, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			log.Println("Error reading UDP datagram: ", err)
+		} else {
+			conn.WriteTo(buf[:n], addr)
+		}
+	}
+
+}
+
+func startUdpServer(port int) error {
+	log.Println("Starting UDP server on port", port)
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return errors.Wrap(err, "Error binding UDP port")
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		return errors.Wrap(err, "Error listenig to UDP port")
+	}
+
+	go echoUdp(conn)
+	return nil
+}
 
 type Config struct {
 	TcpPort int `yaml:"tcp_port"`
@@ -8,6 +44,13 @@ type Config struct {
 }
 
 func Run(config Config) error {
-	fmt.Println("Running server", config)
+	if config.UdpPort != 0 {
+		if err := startUdpServer(config.UdpPort); err != nil {
+			return err
+		}
+	}
+
+	// Block forever
+	select {}
 	return nil
 }
